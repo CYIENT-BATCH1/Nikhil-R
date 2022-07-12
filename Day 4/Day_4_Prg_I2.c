9a73581982786a422e4ffa4a6be5f677510af30f
rnikhil54 <108972470+rnikhil54@users.noreply.github.com>
2022-07-12 11:07:04 +0530

    Add files via upload

---
+++ Day 4/Day_4_Prg_I2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+	int n,n1,clear;
+	printf("enter the number\n");
+	scanf("%d",&n);
+        printf("enter the bit to set\n");
+	scanf("%d",&n1);
+	clear=n&(~(1 << n1));
+	printf("before clear the bit = %d  %d",n1,n);
+	printf("\n after clear the bit = %d  %d",n1,clear);
+	return 0;
+}
