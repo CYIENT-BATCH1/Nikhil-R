f3a14fdac01fc24141b6b29eb03b662d6318b9aa
rnikhil54 <108972470+rnikhil54@users.noreply.github.com>
2022-07-12 09:40:52 +0530

    Delete Day_1_Prg_2.c

--- Day_1_Prg_2.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
-{
-char ch;
-printf("enter the character");
-scanf("%c",&ch);
-(ch=='a'||ch=='A'||ch=='e'||ch=='E'||ch=='i'||ch=='I'||ch=='o'||ch=='O'||ch=='u'||ch=='U')?
-printf("\n %c is vowel",ch):printf("\n %c is consonant",ch);
-}
